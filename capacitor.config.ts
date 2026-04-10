7cb49f2fb95a1d2033147c367a1503e8353bafbf
Neha Bairagi <neha.bairagi@beyondkey.com>
2026-04-10 13:36:00 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MyStorageNotificationApp',
+  webDir: 'www'
+};
+
+export default config;
